955e9cb60b504c66d2c1f31d635093a74b0f741b
hachi8877 <shinya19970712@icloud.com>
2021-04-07 11:16:15 +0000

    コード読みやすく2

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :background_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
